59587f2d009d5557885a7e6920a8949ce023bd71
VinLvy <davinputrafibrian24@gmail.com>
2024-11-09 10:32:16 +0700

    Sitemap 1.1

--- app/Controllers/user/Sitemapctrl.php
+++ app/Controllers/user/Sitemapctrl.php
@@ -25,10 +25,10 @@ class Sitemapctrl extends BaseController
         $data = [
             'home_id' => base_url('id'),
             'home_en' => base_url('en'),
-            'about_id' => base_url('id/tentang-kami'),
+            'about_id' => base_url('id/tentang'),
             'about_en' => base_url('en/about'),
-            'product_id' => base_url('id/materi-pelatihan'),
-            'product_en' => base_url('en/training-topics'),
+            'product_id' => base_url('id/layanan'),
+            'product_en' => base_url('en/service'),
             'article_id' => base_url('id/blog'),
             'article_en' => base_url('en/blogs'),
             'activity_id' => base_url('id/klien'),
@@ -39,20 +39,20 @@ class Sitemapctrl extends BaseController
 
         // URL dinamis untuk aktivitas
         foreach ($aktivitasData as $aktivitas) {
-            $data['aktivitas_id'][] = base_url('id/klien/' . $aktivitas->slug_id);
-            $data['aktivitas_en'][] = base_url('en/clients/' . $aktivitas->slug_en);
+            $data['aktivitas_id'][] = base_url('id/aktivitas/' . $aktivitas->slug_id);
+            $data['aktivitas_en'][] = base_url('en/activities/' . $aktivitas->slug_en);
         }
 
         // URL dinamis untuk produk
         foreach ($productsData as $product) {
-            $data['products_id'][] = base_url('id/materi-pelatihan/' . $product->slug_id);
-            $data['products_en'][] = base_url('en/training-topics/' . $product->slug_en);
+            $data['products_id'][] = base_url('id/layanan/' . $product->slug_id);
+            $data['products_en'][] = base_url('en/service/' . $product->slug_en);
         }
 
         // URL dinamis untuk artikel
         foreach ($artikelData as $artikel) {
-            $data['articles_id'][] = base_url('id/blog/' . $artikel->slug_id);
-            $data['articles_en'][] = base_url('en/blogs/' . $artikel->slug_en);
+            $data['articles_id'][] = base_url('id/artikel/' . $artikel->slug_id);
+            $data['articles_en'][] = base_url('en/articles/' . $artikel->slug_en);
         }
 
         // Load view dengan data yang diperlukan untuk sitemap
--- app/Views/sitemap.php
+++ app/Views/sitemap.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sitemap - Competent Academy</title>
+    <title>Sitemap - Elecomp Indonesia</title>
     <style>
         body {
             font-family: Arial, sans-serif;
